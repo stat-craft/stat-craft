c39061fb145c934a7fcf04286ccab4a0bf7baa0b
Mariusz <mariusz9dabal@gmail.com>
2024-10-08 18:50:05 +0200

    STC-6-Dev-Conf

---
+++ backend/statCraft/views.py
@@ -0,0 +1,8 @@
+import os
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class Index(APIView):
+    def get(self, request):
+        return Response(os.getenv("APP_NAME"))
--- statCraft/statCraft/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-import os
-
-
-def index(request):
-    return HttpResponse(os.getenv("APP_NAME") + " gej.")

ddf4ff0fad6ef5b873b8ba7384176d00d13d5ac1
Mariusz <mariusz9dabal@gmail.com>
2024-10-31 20:07:03 +0100

    STC-6 prod stuff

--- backend/statCraft/views.py
+++ backend/statCraft/views.py
@@ -6,3 +6,6 @@ from rest_framework.response import Response
 class Index(APIView):
     def get(self, request):
         return Response(os.getenv("APP_NAME"))
+
+    def post(self, payload):
+        return "elo"

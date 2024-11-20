969a92213dbd00799d8ae1e45a30ff444fe4cd46
Uaciej <amacedmace@gmail.com>
2024-11-20 12:36:33 +0100

    STC-9 User module lint check

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,3 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
